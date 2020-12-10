68d866c31cfec56bf95f3be3a2f27b12e29f5fd2
Kunal Ashar <kunalashar@Kunals-MacBook-Pro.local>
2020-12-10 17:49:08 +0800

    run info added

--- cypress/integration/learnings/first_webpage_test.js
+++ cypress/integration/learnings/first_webpage_test.js
@@ -16,4 +16,28 @@ describe('My First Web Suite',function(){
         cy.visit('https://example.cypress.io/commands/actions')
         cy.title().should('eq','Cypress.io Kitchen Sink')
     })
+
+    /**
+     * HOW TO EXECUTE?
+     */
+
+    // to open CYPRESS RUNNER
+    // node_modules/.bin/cypress open
+
+    // to run from TERMINAL
+    // node_modules/.bin/cypress run (default behavior is headless mode)
+    // this command will run all the js under example folder on Electron Browser
+
+    // to run with HEADED mode (with open browser)
+    // node_modules/.bin/cypress run --headed
+
+    // to run SPECIFIC FILE
+    // node_modules/.bin/cypress run --spec "cypress/integration/learnings/first_webpage_test.js"
+
+    // to run on CHROME browser
+    // node_modules/.bin/cypress run --browser chrome
+    // chrome browser is by default in headed mode
+
+    // Refer for more COMMAND LINE commands
+    // https://docs.cypress.io/guides/guides/command-line.html#cypress-run
 })
