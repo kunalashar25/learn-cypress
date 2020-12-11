7f747bc11660844740ac74253bd1ded94c8102c5
Kunal Ashar <kunalashar@Kunals-MacBook-Pro.local>
2020-12-11 12:52:43 +0800

    textbox interaction added

---
+++ cypress/integration/learnings/textbox.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress"/>
+
+describe('UI Elements',function(){
+
+    it('Working with Textbox',function(){
+        // open webpage
+        cy.visit("http://demo.guru99.com/test/newtours/index.php")
+
+        // current page url validation
+        cy.url().should('include','newtours')
+
+        // username box
+        cy.get("[name='userName']").should('be.visible').should('be.enabled').type('mercury')
+
+        // password box
+        cy.get("[name=password]").should('be.visible').should('be.enabled').type('mercury')
+
+        // sign in button
+        cy.get("[name=submit]").should('be.visible').should('be.enabled').click()
+    })
+})
