406ba13a3b6fc92be6858e56f0272341885b466c
Kunal Ashar <kunalashar@Kunals-MacBook-Pro.local>
2020-12-22 00:40:14 +0800

    created page object model

---
+++ cypress/integration/learnings/page_object_test.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress"/>
+
+// to use class in testcase
+import LoginPage from '../pageObjectModel/loginPage'
+
+describe('Page Object Model', function () {
+
+    it('POM Test', function () {
+        const loginPage = new LoginPage()
+
+        loginPage.visit()
+        loginPage.fillEmail('testemail')
+        loginPage.fillPassword('password')
+        loginPage.submit()
+    })
+})
---
+++ cypress/integration/pageObjectModel/loginPage.js
@@ -0,0 +1,30 @@
+class LoginPage {
+
+
+    // method to visit the page
+    visit() {
+        cy.visit('https://admin-demo.nopcommerce.com/login')
+    }
+
+    // method to fill email
+    fillEmail(value) {
+        cy.get('input[name=Email]').clear().type(value)
+        return this
+    }
+
+    // method to fill password
+    fillPassword(value) {
+        const field = cy.get('input[name=Password]')
+        field.clear().type(value)
+        return this
+    }
+
+    // to click on login
+    submit(){
+        cy.get('[type=submit]').click()
+        return this
+    }
+}
+// to use this class in all testcases
+// Syntax: export default $className
+export default LoginPage
